659013124d5dbf5b493e5b5070a7caaed267bc19
Jakub Cabala <kuba.cabala6@gmail.com>
2025-02-08 18:00:32 +0000

    fix: Fixed more conflicts

--- contracts/src/AES/AES.ts
+++ contracts/src/AES/AES.ts
@@ -1,8 +1,7 @@
-import { method, SmartContract } from "o1js";
-import { Byte16 } from "../primitives/Bytes";
-import { sbox } from "./SBox";
-import { shiftRows } from "./ShiftRows"
-
+import { method, SmartContract } from 'o1js';
+import { Byte16 } from '../primitives/Bytes';
+import { sbox } from './SBox';
+import { shiftRows } from './ShiftRows';
 
 class AES extends SmartContract {
   @method.returns(Byte16)
@@ -15,40 +14,24 @@ class AES extends SmartContract {
     return input;
   }
 
-<<<<<<< Updated upstream
-    @method.returns(Byte16)
-    async sbox(input: Byte16): Promise<Byte16> {
-        // Get top 64 bits
-        const enc_top = sbox(input.top);
-        const enc_bot = sbox(input.bot);
-=======
   @method.returns(Byte16)
   async sbox(input: Byte16): Promise<Byte16> {
     // Get top 64 bits
-
     const enc_top = sbox(input.top);
     const enc_bot = sbox(input.bot);
->>>>>>> Stashed changes
 
     return new Byte16(enc_top, enc_bot);
   }
 
-<<<<<<< Updated upstream
-    @method.returns(Byte16)
-    async mixColumns(input: Byte16): Promise<Byte16> {
-        return input;
-    }
-    
-    @method.returns(Byte16)
-    async shiftRows(input: Byte16): Promise<Byte16> {
-        return shiftRows(input);
-    }
-=======
+  @method.returns(Byte16)
+  async mixColumns(input: Byte16): Promise<Byte16> {
+    return input;
+  }
+
   @method.returns(Byte16)
   async shiftRows(input: Byte16): Promise<Byte16> {
     return shiftRows(input);
   }
->>>>>>> Stashed changes
 }
 
 export { AES };
--- contracts/src/AES/MixColumns.ts
+++ contracts/src/AES/MixColumns.ts
@@ -1,10 +1,17 @@
-import { Circuit, Field, Gadgets, Bool, circuitMain, Provable, MerkleList } from "o1js";
-import { Byte16 } from "../primitives/Bytes"
-import { FieldList } from "../utils/list";
+import {
+  Circuit,
+  Field,
+  Gadgets,
+  Bool,
+  circuitMain,
+  Provable,
+  MerkleList,
+} from 'o1js';
+import { Byte16 } from '../primitives/Bytes';
+import { FieldList } from '../utils/list';
 
 function MixColumn(input: Byte16): Field {
-    return input.top;
-
+  return input.top;
 }
 
 function xor8(a: Field, b: Field): Field {
@@ -15,72 +22,23 @@ function xor8_5(a: Field, b: Field, c: Field, d: Field, e: Field): Field {
   return xor8(xor8(xor8(xor8(a, b), c), d), e);
 }
 
-<<<<<<< Updated upstream
 export function gmixColumn(r: FieldList): FieldList {
-    let a = FieldList.empty();
-    let b = FieldList.empty();
-    
-    // Process each byte in the column.
-    let iterator = r.startIterating();
-    for (let c = 0; c < 4; c++) {
-      let value = iterator.next();
-      value.assertLessThanOrEqual(Field(0xff));
-      
-      a.push(value);
-        
-      // Compute h = r[c] & 0x80.
-      const h = Gadgets.and(value, Field(0x80), 8).equals(Field(0x80));
-      
-      // Compute b[c] = r[c] << 1, i.e. multiply by 2.
-      let b_val = Gadgets.leftShift32(value, 1);
-
-      // TODO: zk magic to prevent inflating circuit
-      b_val = Provable.if(h, Gadgets.xor(b_val, Field(0x1B), 8), b_val);
-    
-      b.push(Gadgets.and(b_val, Field(0xff), 8));
-    }
-
-    // Harcode :(
-    const b_3 = b.pop();
-    const b_2 = b.pop();
-    const b_1 = b.pop();
-    const b_0 = b.pop();
-
-    const a_3 = a.pop();
-    const a_2 = a.pop();
-    const a_1 = a.pop();
-    const a_0 = a.pop();
-    
-    // Now compute the new column bytes.
-    // The C code uses XOR (^) to combine values.
-    // We assume our helper xor8 (and xor8_5) performs an 8-bit XOR.
-    const r0 = xor8_5(b_0, a_3, a_2, b_1, a_1);
-    const r1 = xor8_5(b_1, a_0, a_3, b_2, a_2);
-    const r2 = xor8_5(b_2, a_1, a_0, b_3, a_3);
-    const r3 = xor8_5(b_3, a_2, a_1, b_0, a_0);
-
-    return FieldList.from([r0, r1, r2, r3]);
-}
-=======
-// TODO: Modify Field[] into MerkleListBase
-export function gmixColumn(r: Field[]): Field[] {
-  if (r.length !== 4) {
-    throw new Error('gmixColumn: expected array of 4 bytes');
-  }
-  let a: Field[] = [];
-  let b: Field[] = [];
+  let a = FieldList.empty();
+  let b = FieldList.empty();
 
   // Process each byte in the column.
+  let iterator = r.startIterating();
   for (let c = 0; c < 4; c++) {
-    r[c].assertLessThanOrEqual(Field(0xff));
+    let value = iterator.next();
+    value.assertLessThanOrEqual(Field(0xff));
 
-    a.push(r[c]);
+    a.push(value);
 
     // Compute h = r[c] & 0x80.
-    const h = Gadgets.and(r[c], Field(0x80), 8).equals(Field(0x80));
+    const h = Gadgets.and(value, Field(0x80), 8).equals(Field(0x80));
 
     // Compute b[c] = r[c] << 1, i.e. multiply by 2.
-    let b_val = Gadgets.leftShift32(r[c], 1);
+    let b_val = Gadgets.leftShift32(value, 1);
 
     // TODO: zk magic to prevent inflating circuit
     b_val = Provable.if(h, Gadgets.xor(b_val, Field(0x1b), 8), b_val);
@@ -88,14 +46,24 @@ export function gmixColumn(r: Field[]): Field[] {
     b.push(Gadgets.and(b_val, Field(0xff), 8));
   }
 
+  // Harcode :(
+  const b_3 = b.pop();
+  const b_2 = b.pop();
+  const b_1 = b.pop();
+  const b_0 = b.pop();
+
+  const a_3 = a.pop();
+  const a_2 = a.pop();
+  const a_1 = a.pop();
+  const a_0 = a.pop();
+
   // Now compute the new column bytes.
   // The C code uses XOR (^) to combine values.
   // We assume our helper xor8 (and xor8_5) performs an 8-bit XOR.
-  const r0 = xor8_5(b[0], a[3], a[2], b[1], a[1]);
-  const r1 = xor8_5(b[1], a[0], a[3], b[2], a[2]);
-  const r2 = xor8_5(b[2], a[1], a[0], b[3], a[3]);
-  const r3 = xor8_5(b[3], a[2], a[1], b[0], a[0]);
+  const r0 = xor8_5(b_0, a_3, a_2, b_1, a_1);
+  const r1 = xor8_5(b_1, a_0, a_3, b_2, a_2);
+  const r2 = xor8_5(b_2, a_1, a_0, b_3, a_3);
+  const r3 = xor8_5(b_3, a_2, a_1, b_0, a_0);
 
-  return [r0, r1, r2, r3];
+  return FieldList.from([r0, r1, r2, r3]);
 }
->>>>>>> Stashed changes
