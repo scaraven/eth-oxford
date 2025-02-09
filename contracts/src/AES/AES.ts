190edbcc1416d9e6bbbe24a042b335ca971f4e6a
scaraven <sc4raven@gmail.com>
2025-02-09 00:15:12 +0000

    test: implement dummy proof for AES smart contract

--- contracts/src/AES/AES.ts
+++ contracts/src/AES/AES.ts
@@ -1,38 +1,17 @@
-import { method, Proof, SmartContract } from "o1js";
+import { Proof, SmartContract, Struct } from "o1js";
 import { Byte16 } from "../primitives/Bytes.js";
-import { sbox } from "./SBox.js";
-import { shiftRows } from "./ShiftRows.js"
-import { FieldList } from "../utils/list.js";
-import { gmixColumn, mixColumn } from "./MixColumns.js";
 
-class AESProof extends Proof<Byte16, Byte16> {};
+class AESProof extends Proof<AESPublicInput, void> {};
+
+class AESPublicInput extends Struct({
+    cipher: Byte16,
+  }) {}
 
 class AES extends SmartContract {
 
-  @method.returns(Byte16)
-  async encrypt(input: Byte16): Promise<Byte16> {
-    return input;
+  async verify(input: AESProof) {
+    input.verify();
   }
-
-  @method.returns(Byte16)
-  async decrypt(input: Byte16): Promise<Byte16> {
-    return input;
-  }
-
-  @method.returns(Byte16)
-  async sbox(input: Byte16): Promise<Byte16> {
-    return sbox(input);
-  }
-
-    @method.returns(Byte16)
-    async mixColumns(input: Byte16): Promise<Byte16> {
-        return mixColumn(input);
-    }
-    
-    @method.returns(Byte16)
-    async shiftRows(input: Byte16): Promise<Byte16> {
-        return shiftRows(input);
-    }
 }
 
-export { AES, AESProof };
+export { AES, AESProof, AESPublicInput};
